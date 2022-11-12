662ef35cc7ac43cf7f8b272175d8c6d9ca6bbf94
Gustavo Kumagai <iagamuk.gus@gmail.com>
2022-11-12 08:46:20 -0300

    Add Tasks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import './App.css';
+import TasksManager from "./components/TasksManager";
 
 const App = () => {
-  const mensagem = 'Hello World!'
+  // const message = 'Hello World!'
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar Programação",
+      completed: true
+    },
+    {
+      id: 2,
+      title: "Continuar TCC",
+      completed: false
+    }
+  ]);
 
-  return <div className="container">
-
-  </div>;
+  return (
+    <>
+      <div className="container">
+        <TasksManager tasks={tasks}/>
+      </div>
+    </>
+  );
 };
 
 export default App;
