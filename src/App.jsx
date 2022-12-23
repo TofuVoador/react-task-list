c42847302d8c49b7f03a840c65e4e297391290e0
Gustavo Kumagai <iagamuk.gus@gmail.com>
2022-12-23 00:03:57 -0300

    Persist Data

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,14 @@ import Header from "./components/Header";
 import TaskDetails from "./components/TaskDetails";
 
 const App = () => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([])
+
+  var t = localStorage.getItem('tasks') || [];
+  setTasks(t)
+
+  const save = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
 
   const handleTaskClick = (taskId) => {
     const newTasks = tasks.map(task => {
@@ -19,6 +26,7 @@ const App = () => {
     })
 
     setTasks(newTasks);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
   }
 
   const handleDescribeTask = (taskId, taskDesc) => {
@@ -29,6 +37,7 @@ const App = () => {
     })
 
     setTasks(newTasks);
+
   }
 
   function handleGetTaskDescription (taskId) {
@@ -48,31 +57,36 @@ const App = () => {
     ];
 
     setTasks(newTasks);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
   }
 
   const handleTaskDelete = (taskId) => {
     const newTasks = tasks.filter(task => task.id !== taskId);
 
     setTasks(newTasks);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
   }
 
   return (
-    <Router>
-      <div className="container">
-        <Route path="/" exact render={() => (
-          <>
-            <Header/>
-            <AddTask handleTaskAddition={handleTaskAddition}/>
-            <TasksManager tasks={tasks} handleTaskClick={handleTaskClick} handleTaskDelete={handleTaskDelete}/>
-          </>
-        )}/>
-        <Route path="/:taskId" exact render={() => (
-          <>
-            <TaskDetails handleDescribeTask={handleDescribeTask} handleGetTaskDescription={handleGetTaskDescription}/>
-          </>
-        )}/>
-      </div>
-    </Router>
+    <>
+      <a>a</a>
+      <Router>
+        <div className="container">
+          <Route path="/" >
+            <>
+              <Header/>
+              <AddTask handleTaskAddition={handleTaskAddition}/>
+              <TasksManager tasks={tasks} handleTaskClick={handleTaskClick} handleTaskDelete={handleTaskDelete}/>
+            </>
+          </Route>
+          <Route path="/:taskId">
+            <>
+              <TaskDetails handleDescribeTask={handleDescribeTask} handleGetTaskDescription={handleGetTaskDescription}/>
+            </>
+          </Route>
+        </div>
+      </Router>
+    </>
   );
 };
 
